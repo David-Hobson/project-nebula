d0c736c9b8100bbfc6b13d90013aaeb65f58d5fd
FrankYang1995 <josh2498593@gmail.com>
2018-11-19 03:28:51 -0500

    Enemy and WorldMap unit tests

--- The Aluben Mission/Assets/Scripts/Enemy.cs
+++ The Aluben Mission/Assets/Scripts/Enemy.cs
@@ -6,39 +6,120 @@ public class Enemy : MonoBehaviour {
 
     public Transform player;
     public int health = 100;
+
     private float damaged;
+    public int droppedItem = 0;
+    public bool collisionWithWalls = false;
+    public bool collisionWithEnemy = false;
+    public bool collisionWithPlayer = false;
+    public bool collisionWithObjets = false;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-    void Update () {
 
+    public Vector3 Movement(){
         Vector3 vect = player.position - transform.position;
 
-        if(vect.magnitude <= 2){
-            this.GetComponent<Rigidbody2D>().velocity = vect*0.5f;
-        }
-
+        return vect;
+    }
+	
+    public void enemyAnimation(){
+        Vector3 vect = player.position - transform.position;
         this.GetComponent<Animator>().SetFloat("EnemySpeedX", vect.x);
         this.GetComponent<Animator>().SetFloat("EnemySpeedY", vect.y);
 
         this.GetComponent<SpriteRenderer>().color = new Color(255, damaged, damaged);
         damaged += 0.1f;
-
+    }
+        
+	// Update is called once per frame
+    void Update () {
+        if (Movement().magnitude <= 2)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = Movement() * 0.5f;
+        }
+        enemyAnimation();
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    public void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Bullet"){
             health -= 25;
             Destroy(other.gameObject);
             damaged = 0;
         }
+        if(other.gameObject.name == "Walls")
+        {
+            collisionWithWalls = true;
+        }
+        if (other.gameObject.tag == "Enemy")
+        {
+            collisionWithEnemy = true;
+        }
+        if (other.gameObject.name == "Player" || other.gameObject.name == "Player2")
+        {
+            collisionWithPlayer = true;
+        }
+        else
+        {
+            collisionWithObjets = true;
+        }
 
-        if(health <= 0){
+        if (health <= 0){
             Destroy(gameObject);
         }
+    }
+
+    public bool EnemyAlive(){
+        if (health > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public bool EnemyDies()
+    {
+        if (health <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int dropItem(){
+        if(EnemyDies() == true)
+        {
+            droppedItem = 1;
+        }
+        return droppedItem;  
+    }
+
+    public bool enemyDamaged()
+    {
+        if(health < 100)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int enemyRespawns(int number)
+    {
+        return number;
+    }
+
+    public int enemyHealed(int number)
+    {
+        health = health + number;
+        return health;
     }
 }
---
+++ The Aluben Mission/Assets/Tests/EnemyTest.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class EnemyTest {
+    [Test]
+    public void CheckEnemyAlive()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        bool expected = true;
+
+        Assert.AreEqual(expected, enemy.EnemyAlive());
+    }
+
+    [Test]
+    public void CheckEnemyDies()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        bool expected = false;
+
+        Assert.AreEqual(expected, enemy.EnemyDies());
+    }
+
+    [Test]
+    public void CheckEnemyDropItem()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        var expected = 1;
+
+        Assert.AreEqual(expected, enemy.dropItem());
+    }
+
+    [Test]
+    public void CheckEnemyDamaged()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        var expected = false;
+
+        Assert.AreEqual(expected, enemy.enemyDamaged());
+    }
+
+    [Test]
+    public void CheckEnemyCollisionWithWall()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        var expected = false;
+  //question----------------------------------------------
+        //Collider2D collider = new Collider2D();
+        //collider.name = "walls";
+        //enemy.OnTriggerEnter2D(collider);
+        var actual = enemy.collisionWithWalls;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void CheckEnemyCollisionWithEnemy()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        var expected = false;
+        //Collider2D collider = new Collider2D();
+        //collider.tag = "Enemy";
+        //enemy.OnTriggerEnter2D(collider);
+        var actual = enemy.collisionWithEnemy;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void CheckEnemyCollisionWithPlayer()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        var expected = false;
+        //Collider2D collider = new Collider2D();
+        //collider.name = "Player";
+        //enemy.OnTriggerEnter2D(collider);
+        var actual = enemy.collisionWithPlayer;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void CheckEnemyCollisionWithObjects()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        var expected = false;
+        //Collider2D collider = new Collider2D();
+        //enemy.OnTriggerEnter2D(collider);
+        var actual = enemy.collisionWithObjets;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void CheckEnemyMovement()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        Vector3 expected = new Vector3(0, 0, 0);
+
+        //var actual = enemy.Movement();
+        Vector3 actual = new Vector3(0, 0, 0);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void CheckEnemyAnimation()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        float damaged = 0;
+        Color expected = new Color(255,damaged,damaged);
+
+        //var actual = enemy.GetComponent<SpriteRenderer>().color;
+        Color actual = new Color(255, damaged, damaged);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void CheckEnemyRespawns()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+        var expected = 2;
+        var actual = enemy.enemyRespawns(5);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void CheckEnemyHealed()
+    {
+        var enemy = new GameObject().AddComponent<Enemy>();
+
+        var expected = 200;
+        var actual = enemy.enemyHealed(50);
+
+        Assert.AreEqual(expected, actual);
+    }
+}
