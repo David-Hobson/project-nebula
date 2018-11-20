1a1fafc829690d320f8d7c0117d952f45a6c04db
David Hobson <davidhobson96@gmail.com>
2018-11-20 11:44:12 -0500

    Add test and dummy scripts for NPC, Nebulite, Inventory, Item, Shop, and Weapon.

---
+++ The Aluben Mission/Assets/Resources/Scripts/Tests/NPCTests.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NPCTests
+{
+
+    [Test]
+    public void CheckInteractionWithPlayer()
+    {
+        var character = new GameObject().AddComponent<NPC>();
+        character.startDialogue();
+        Assert.AreEqual(character.checkStatus(), true);
+    }
+
+    [Test]
+    public void CheckNPCEnd()
+    {
+        var character = new GameObject().AddComponent<NPC>();
+        character.startDialogue();
+        character.endDialogue();
+        Assert.AreEqual(character.checkStatus(), false);
+    }
+}
---
+++ The Aluben Mission/Assets/Resources/Scripts/Tests/ShopTests.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ShopTests
+{
+
+    [Test]
+    public void CheckItemPurchaseSuccess()
+    {
+        var shop = new GameObject().AddComponent<Shop>();
+        int[] index = { 0, 0 };
+        shop.AddItem(100, 1, index, true);
+        Assert.AreEqual(shop.PurchaseItem(0), true);
+    }
+
+    [Test]
+    public void CheckItemPurchaseFailure()
+    {
+        var shop = new GameObject().AddComponent<Shop>();
+        int[] index = { 0, 0 };
+        shop.AddItem(100, 1, index, true);
+        Assert.AreEqual(shop.PurchaseItem(0), false);
+    }
+
+    [Test]
+    public void CheckItemRemovedOnPurchase()
+    {
+        var shop = new GameObject().AddComponent<Shop>();
+        int[] index = { 0, 0 };
+        Item item = new Item(100, 1, index, true);
+        shop.AddItem(item);
+        shop.PurchaseItem(0);
+        Assert.AreEqual(shop.items.IndexOf(item), -1);
+    }
+
+    [Test]
+    public void CheckNebuliteRemovedOnPurchase()
+    {
+        var shop = new GameObject().AddComponent<Shop>();
+        int ammount = shop.nebulite.GetTotal();
+        int[] index = { 0, 0 };
+        shop.AddItem(100, 1, index, true);
+        shop.PurchaseItem(0);
+        Assert.AreEqual(shop.nebulite.GetTotal(), ammount - 100);
+
+    }
+
+    [Test]
+    public void CheckItemAddedToInventory()
+    {
+        var shop = new GameObject().AddComponent<Shop>();
+        int[] index = { 0, 0 };
+        Item item = new Item(100, 1, index, true);
+        shop.AddItem(item);
+        shop.PurchaseItem(0);
+        Assert.AreNotEqual(shop.inventory.items.IndexOf(item), -1);
+    }
+
+    [Test]
+    public void CheckWeaponAddedToInventory()
+    {
+        var shop = new GameObject().AddComponent<Shop>();
+        int[] index = { 0, 0 };
+        Weapon weapon = new Weapon(100, 1, index, true);
+        shop.AddWeapon(weapon);
+        shop.PurchaseWeapon(0);
+        Assert.AreEqual(shop.inventory.weapons[0].aquired, true);
+    }
+
+    [Test]
+    public void CheckUpgradeApplied()
+    {
+        var shop = new GameObject().AddComponent<Shop>();
+        int[] index = { 0, 0 };
+        Weapon weapon = new Weapon(0, 1, index, true);
+        shop.AddWeapon(weapon);
+        shop.PurchaseWeapon(0);
+        shop.PurchaseWeapon(0);
+        Assert.AreEqual(shop.inventory.weapons[0].tier, 2);
+    }
+
+
+}
