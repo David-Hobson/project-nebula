e302c70cfcde50317e2d66527ac53abbb29791e5
David Hobson <davidhobson96@gmail.com>
2018-11-20 11:53:31 -0500

    Create test and dummy scripts for Level, Cutscene, Puzzle, and UI.

---
+++ The Aluben Mission/Assets/Resources/Scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    public int LevelActive()
+    {
+        return 1;
+    }
+
+    public int LevelNotComplete()
+    {
+        return 2;
+    }
+
+    public int LevelComplete()
+    {
+        return 3;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
