9b29c5d0d951bf9cbe52eeaaaf229548a77f6935
Zuodong Xie <Jerry.X.Xie@outlook.com>
2018-12-10 00:15:46 -0500

    add UI to develop

---
+++ The Aluben Mission/Assets/Resources/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+
+    //take an integer parameter called sceneIndex, load it when it's called
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ The Aluben Mission/Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour
+{
+
+
+    //if game is playing, set false; or quit the application
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
